6f81d8362265ef32528193d1fa3269571bae1fb2
jasa <jasa.ksela@student.um.si>
2019-05-06 12:31:56 +0200

    par stvari poravljeno, se vendo ne dela pravilno

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 import numpy as np
-import math
 
 
-
-def trilateration(P1, P2, P3, R1, R2, R3):
+def trilateration(P1, P2, P3, r1, r2, r3):
     p1 = np.array([0, 0, 0])
     p2 = np.array([P2[0] - P1[0], P2[1] - P1[1], P2[2] - P1[2]])
     p3 = np.array([P3[0] - P1[0], P3[1] - P1[1], P3[2] - P1[2]])
@@ -11,35 +9,33 @@ def trilateration(P1, P2, P3, R1, R2, R3):
     v1 = p2 - p1
     v2 = p3 - p1
 
-    Xn = v1 / np.sqrt(np.sum(v1**2))
+    Xn = (v1)/np.linalg.norm(v1)
 
-    Z = np.cross(v1, v2)
-    Zn = Z / np.sqrt(np.sum(Z ** 2))
+    tmp = np.cross(v1, v2)
+
+    Zn = (tmp)/np.linalg.norm(tmp)
 
     Yn = np.cross(Xn, Zn)
 
-    d = np.dot(Xn, v1)
     i = np.dot(Xn, v2)
+    d = np.dot(Xn, v1)
     j = np.dot(Yn, v2)
 
-    X = (((R1 ** 2) - (R2 ** 2) + (d ** 2))) / (2 * d)
-    Y = ((R1 ** 2 - R3 ** 2 + i ** 2+ j**2)) / ((2 * j)-(i/j)*X)
-
-    a = abs(R1**2 - X ** 2 - Y ** 2)
-
-    Z1 = math.sqrt(a)
-    Z2 = -math.sqrt(abs(R1 ** 2 - X ** 2 - Y ** 2))
+    X = ((r1**2)-(r2**2)+(d**2))/(2*d)
+    Y = (((r1**2)-(r3**2)+(i**2)+(j**2))/(2*j))-((i/j)*(X))
+    Z1 = np.sqrt(r1**2-X**2-Y**2)
+    Z2 = np.sqrt(r1**2-X**2-Y**2)*(-1)
 
     K1 = P1 + X*Xn + Y * Yn + Z1 * Zn
-    K2 = P1 + X * Xn + Y * Yn - Z2 * Zn
+    K2 = p1 + X * Xn + Y * Yn - Z2 * Zn
 
 
 
 if __name__ == "__main__":
     # PRIMER 1
     vector1, vector2, vector3 = np.array([2, 1, 0]), np.array([4, 3, 0]), np.array([4, 4, 1])
-    r1 = 2
-    r2 = 2
-    r3 = 2.449
+    R1 = 2
+    R2 = 2
+    R3 = 2.449
 
-    trilateration(vector1, vector2, vector3, r1, r2, r3)
+    trilateration(vector1, vector2, vector3, R1, R2, R3)
